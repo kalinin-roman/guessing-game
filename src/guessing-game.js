11cc88fbc1bd5a89911cc0b7d3ded1f3bbe40321
Roman Kalinin <pancu43@gmail.com>
2023-10-01 08:59:12 +0500

    guessing-game is done

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,22 @@
 class GuessingGame {
-    constructor() {}
-
+    constructor() {
+        this.min = 0;
+        this.max = 0;
+    }
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
-
     guess() {
-
+        return Math.ceil((this.max + this.min) / 2);
     }
-
-    lower() {
-
-    }
-
     greater() {
-
+        alert(this.min = this.guess())
+        this.min = this.guess();
+    }
+    lower() {
+        alert(this.max = this.guess())
+        this.max = this.guess();
     }
 }
-
 module.exports = GuessingGame;
